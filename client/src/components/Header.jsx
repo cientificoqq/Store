521aed059957f0a80fbf8272d6eeb6a89e705869
ren <93995954+cientificoqq@users.noreply.github.com>
2025-02-20 00:08:59 +0200

    Modal

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,117 +1,70 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
+import { Context } from "..";
+import { NavLink } from "react-router-dom";
+import { SHOP_ROUTE } from "../utils/consts";
+import { Button, ButtonGroup } from "@mui/material";
+import { observer } from "mobx-react-lite";
+import Auth from "../pages/Auth";
 
-const pages = ["Products", "Pricing", "Blog"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+const Header = observer(() => {
+  const { user } = React.useContext(Context);
+  const [openAuthModal, setOpenAuthModal] = React.useState(false);
 
-const Header = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+  const handleOpenModal = () => setOpenAuthModal(true);
+  const handleCloseModal = () => setOpenAuthModal(false);
 
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
+    <>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar
+            disableGutters
+            sx={{ display: "flex", justifyContent: "space-between" }}
           >
-            LOGO
-          </Typography>
+            <div>
+              <h1>
+                <NavLink
+                  style={{ color: "white", textDecoration: "none" }}
+                  to={SHOP_ROUTE}
+                >
+                  Store
+                </NavLink>
+              </h1>
+            </div>
+            {user.isAuth ? (
+              <ButtonGroup>
+                <Button variant="text" color="default">
+                  Admin
+                </Button>
+                <Button
+                  variant="text"
+                  color="default"
+                  onClick={() => user.setIsAuth(false)}
+                >
+                  Log out
+                </Button>
+              </ButtonGroup>
+            ) : (
+              <ButtonGroup>
+                <Button
+                  variant="text"
+                  color="default"
+                  onClick={handleOpenModal}
+                >
+                  Registration
+                </Button>
+              </ButtonGroup>
+            )}
+          </Toolbar>
+        </Container>
+      </AppBar>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              anchorEl={anchorElNav}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: "block", md: "none" } }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="User Avatar" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              anchorEl={anchorElUser}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+      <Auth open={openAuthModal} onClose={handleCloseModal} />
+    </>
   );
-};
+});
 
 export default Header;
--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,10 +1,60 @@
-import React from "react";
+import { Box, Button, Modal, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
 
-const Auth = () => {
+const Auth = ({ open, onClose }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Name:", name, "Email:", email);
+    onClose();
+  };
   return (
-    <div>
-      <h1>Auth</h1>
-    </div>
+    <Modal open={open} onClose={onClose}>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: 400,
+          bgcolor: "background.paper",
+          boxShadow: 24,
+          p: 4,
+          borderRadius: 2,
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+        }}
+      >
+        <Typography variant="h6" component="h2">
+          Введіть ваші дані
+        </Typography>
+        <TextField
+          label="Ім'я"
+          variant="outlined"
+          fullWidth
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <TextField
+          label="Email"
+          type="email"
+          variant="outlined"
+          fullWidth
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Надіслати
+        </Button>
+      </Box>
+    </Modal>
   );
 };
 
